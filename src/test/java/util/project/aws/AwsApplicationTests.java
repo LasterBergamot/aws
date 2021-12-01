687e0b9a283dda386f67457aae5f31c6e42e18ae
Andras Torok <torokandras1996@gmail.com>
2021-12-01 11:28:20 +0100

    Initial commit.

---
+++ src/test/java/util/project/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package util.project.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
