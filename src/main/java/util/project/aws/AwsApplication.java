2dc4ad1cca2d8ccf810caf9b7af8faab1a02aa83
Andras Torok <torokandras1996@gmail.com>
2021-12-01 17:13:32 +0100

    Refactored the code.

--- src/main/java/util/project/aws/AwsApplication.java
+++ src/main/java/util/project/aws/AwsApplication.java
@@ -4,11 +4,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3VersionSummary;
-import com.amazonaws.services.s3.model.VersionListing;
 import java.io.File;
 import java.time.Instant;
 import java.util.Date;
@@ -22,53 +18,56 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsApplication implements CommandLineRunner {
 
+    private static final AmazonS3 S3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
+
     public static void main(String[] args) {
         SpringApplication.run(AwsApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        printArgOrder();
+        downloadFile(args);
+    }
+
+    private static void printArgOrder() {
         System.out.printf(
                 "Arg1: %s, Arg2: %s, Arg3: %s\n",
                 "Bucket name",
                 "File name",
                 "Last modified date"
         );
+    }
 
-        String bucketName = args[0];
-        String fileName = args[1];
-        String lastModifiedDate = args[2] + "T00:00:00Z";
+    private static void downloadFile(String... args) {
+        final String bucketName = args[0];
+        final String fileName = args[1];
+        final Date lastModifiedDate = Date.from(Instant.parse(args[2] + "T00:00:00Z"));
 
-        System.out.printf("Bucket name: %s\n", bucketName);
-        System.out.printf("File name: %s\n", fileName);
-        System.out.printf("Last modified date: %s\n", lastModifiedDate);
-
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
-
-        Date date = Date.from(Instant.parse(lastModifiedDate));
-
-        VersionListing versionListing = s3.listVersions(bucketName, null);
-        List<S3VersionSummary> summaries = versionListing.getVersionSummaries()
-                .stream()
-                .filter(summary -> fileName.equals(summary.getKey()))
-                .filter(summary -> summary.getLastModified().before(date))
-                .collect(Collectors.toUnmodifiableList());
-
-        Optional<S3VersionSummary> latest = summaries
-                .stream()
-                .reduce(
-                        (s3VersionSummary, s3VersionSummary2) ->
-                                s3VersionSummary.getLastModified().after(s3VersionSummary2.getLastModified())
-                                        ? s3VersionSummary
-                                        : s3VersionSummary2
+        getLatestFile(bucketName, fileName, lastModifiedDate)
+                .ifPresentOrElse(
+                        summary -> {
+                            GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, summary.getKey(), summary.getVersionId());
+                            S3.getObject(getObjectRequest, new File("downloaded/" + fileName));
+                        },
+                        () -> System.out.println("Could not download the file!")
                 );
 
-        S3VersionSummary s3VersionSummary = latest.get();
-        GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, s3VersionSummary.getKey(), s3VersionSummary.getVersionId());
-        ObjectMetadata latestObject = s3.getObject(getObjectRequest, new File("downloaded/" + fileName));
+    }
 
-        ObjectListing objectListing = s3.listObjects(bucketName);
+    private static Optional<S3VersionSummary> getLatestFile(String bucketName, String fileName, Date lastModifiedDate) {
+        List<S3VersionSummary> summaries = S3.listVersions(bucketName, null)
+                .getVersionSummaries()
+                .stream()
+                .filter(summary -> fileName.equals(summary.getKey()))
+                .filter(summary -> summary.getLastModified().before(lastModifiedDate))
+                .collect(Collectors.toUnmodifiableList());
 
-        System.out.println();
+        return summaries
+                .stream()
+                .reduce((summary1, summary2) -> summary1.getLastModified().after(summary2.getLastModified())
+                        ? summary1
+                        : summary2
+                );
     }
 }
